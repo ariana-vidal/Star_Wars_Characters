0dd815c69f23099679df3638c3e5b3f5ec3374c6
Ariana Vidal <barbosavidala@gmail.com>
2022-06-15 22:43:42 -0300

    feat: adicionando e estilizando o componente Home

--- details_characters/src/App.vue
+++ details_characters/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app"> 
     <TheHeader />
+    <TheHome />
   </div>
 </template>
 
 <script>
 import './styles/global.css'
 import TheHeader from './components/TheHeader';
+import TheHome from './components/TheHome';
 export default {
   name: 'App',
   components: {
-    TheHeader
+    TheHeader,
+    TheHome
   }
 }
 </script>
---
+++ details_characters/src/components/TheHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: 'TheHome',
+  }
+
+</script>
+
+<style scoped>
+
+main{
+  background-image: linear-gradient(to top, #81AAB3 0%, #800080 100%);
+}
+
+</style>
