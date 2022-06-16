28fe295c533eaf2c8702f72fa7e71134ff8d4258
Ariana Vidal <barbosavidala@gmail.com>
2022-06-15 23:19:34 -0300

    feat: adicionando componente footer

--- details_characters/src/App.vue
+++ details_characters/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app"> 
     <TheHeader />
     <TheHome />
+    <TheFooter />
   </div>
 </template>
 
@@ -9,11 +10,13 @@
 import './styles/global.css'
 import TheHeader from './components/TheHeader';
 import TheHome from './components/TheHome';
+import TheFooter from './components/TheFooter';
 export default {
   name: 'App',
   components: {
     TheHeader,
-    TheHome
+    TheHome,
+    TheFooter
   }
 }
 </script>
---
+++ details_characters/src/components/TheFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <p>
+      <b>“Seu foco determina sua realidade.”</b>
+    </p>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'TheFooter',
+  }
+</script>
+
+<style scoped>
+  footer{
+    width: 100%;
+    height: 60px;
+    background-image: linear-gradient(to top, #964b00 0%, #000000 100%);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+p{
+  color: var(--color-font)
+}
+
+
+</style>
